6a144ed0ec9767b99579724f845792d77aed7936
youngsoo <youngsoo.kim94@gmail.com>
2020-03-17 20:27:46 +0900

    testing gamma value of material constant

--- BimetalNW/wg_long.c
+++ BimetalNW/wg_long.c
@@ -7,7 +7,6 @@ int main(int argc, char **argv)
 	float wx = atof(argv[2]); //width of dielectrics
     float wy = wx;
     float b0 = 150; //distance bet. surface of NW to PML (metal)
-    float sourceY = atof(argv[3]);
 
     float domLengX = (wx+b0*2)/2; //define domain scale parameter
     float domLengY = (wy+b0*2)/2;
@@ -15,10 +14,12 @@ int main(int argc, char **argv)
     float sPos = wx*0.5-5;
     float resParam = wx*0.03;
 
+    float nLoop = atof(argv[3]);
+
     res Res = {resParam/2, {resParam, resParam, 4*resParam}, {1240}}; //variable grid by width of wg 
     dom Dom = {{domLengX}, {domLengY}, {-2000, 3000}};  
     sur Sur = {{SYM, PML}, {SYM, PML}, {PML}, {24}}; //su MyOldCode 1000/k
-    world W = createWorld(Dom, Res, Sur, "%s_lambd%.0f_w%.0f_dx%.1f", argv[0],lambda, wx, resParam);
+    world W = createWorld(Dom, Res, Sur, "%s_lambd%.0f_w%.0f_dx%.1f_N%.0f", argv[0],lambda, wx, resParam, nLoop);
    
     //input object
     object Ag_Side = {Box, {{-INF, INF}, {-INF, INF}, {0, INF}}}; //metal
@@ -29,14 +30,14 @@ int main(int argc, char **argv)
 	object Au_wire = {Difference, {2}, objects {Au_Side, NW_wg}};
 
     // material def. for alis 1.0.2
-    matter Drude_Ag = {{4.07666}, {9.2186, 0}};
-    matter Drude_Au = {{10.48449}, {9.0540, 0}};
+    matter Drude_Ag = {{4.07666}, {9.2186, 0.02776}};
+    matter Drude_Au = {{10.48449}, {9.0540, 0.07750}};
 
     //input objects in world
  	putObjects(W, Drude_Ag, Ag_wire, Drude_Au, Au_wire, n(2.6));
 
-    guidedWaveZ(W, "Ex2", Ey, sourceY, Sine, lambda, 100, 1, 0);
-    guidedWaveZ(W, "Ez2", Ex, sourceY, Sine, lambda, 100, 1, 0);
+    guidedWaveZ(W, "Guided_Mode_Profiles/Ex2", Ey, 3000, Sine, lambda, 100, 1, 0);
+    guidedWaveZ(W, "Guided_Mode_Profiles/Ez2", Ex, 3000, Sine, lambda, 100, 1, 0);
 
     slice XZ = createSliceXZ(W, 0);
 	slice XY = createSliceXY(W, 0);
@@ -54,7 +55,7 @@ int main(int argc, char **argv)
 
 	writeTxt(W, "/Ratio", "Wavelength\tAgOut\tAuOut\tTotal\r\n");
 
-    for (int n = 1, N = 500*lambda/W->dt; timer(n, W->N+N); n++) {//W->N+N
+    for (int n = 1, N = nLoop*lambda/W->dt; timer(n, W->N+N); n++) {//W->N+N
         updateH(W);
 		AgOut -= poyntingZ(W, 200, -wx/2, wx/2, -wx/2, wx/2);
 		AuOut -= poyntingZ(W, -200, -wx/2, wx/2, -wx/2, wx/2); 
@@ -78,11 +79,11 @@ int main(int argc, char **argv)
 			totalOut = 0;
 		}
 		
-        writeRow(W, "/Totaltime", W->dt*n, get(W, Ex, sPos, sourceY ,0));
+        writeRow(W, "/Totaltime", W->dt*n, get(W, Ex, sPos, 3000 ,0));
         
         if (n > W->N) {
-            writeRow(W, "/ExMode", W->dt*n, get(W, Ex, sPos, sourceY, 0));
-            writeSpectrum(W, N, 100, 4500, "/Spectrum", get(W, Ex, sPos, sourceY, 0));
+            writeRow(W, "/ExMode", W->dt*n, get(W, Ex, sPos, 3000, 0));
+            writeSpectrum(W, N, 100, 4500, "/Spectrum", get(W, Ex, sPos, 3000, 0));
             //export data Ex to raw txt files
         }
 
