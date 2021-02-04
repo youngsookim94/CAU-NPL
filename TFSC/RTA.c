5a1170373b8e48879cb8de14b27aaf49526e8981
Youngsoo Kim <youngsoo.kim94@gmail.com>
2021-02-04 15:36:17 +0900

    testing for loop numbers

--- TFSC/RTA.c
+++ TFSC/RTA.c
@@ -12,12 +12,13 @@ int main(int argc, char **argv) {
     float numLayer = atof(argv[2]);
     float lengthY = dUnit*numLayer;
     float sAngle = atof(argv[3]);
+    float nMultiplier = atof(argv[4]);
 
     res RES = {5, {10, 10, 10}, {1240}};
     dom DTF = {{-INF, INF}, {-INF, INF}, {-INF, lengthY/2 + 200}};
-    dom DSF = {{1000}, {0}, {lengthY/2 + 1500}};
+    dom DSF = {{100}, {0}, {lengthY/2 + 1500}};
     sur SUR = {{BBC, lambda*sin(90)}, {BBC, lambda*sin(sAngle)}, {PML}, {24}};
-    world WLD = createWorld(DSF, RES, SUR, "%s_n%.0f_l%.0f_a%.0f", argv[0], numLayer, lambda, sAngle);
+    world WLD = createWorld(DSF, RES, SUR, "%s_n%.0f_l%.0f_a%.0f_N%.0f", argv[0], numLayer, lambda, sAngle, nMultiplier);
 
     object boxSiO2 = {Box, {{-INF, INF}, {-INF, INF}, {-150, 40}}};
     object boxTiO2 = {Box, {{-INF, INF}, {-INF, INF}, {40, 150}}};
@@ -41,7 +42,7 @@ int main(int argc, char **argv) {
 
     writeTxt(WLD, "/RTA", "Wavelength\tReflection\tTransmission\tAbsorption\tTotal\r\n");
 
-    for (int n = 1, N = 200*lambda/WLD -> dt; timer(n, N); n++) {
+    for (int n = 1, N = nMultiplier*lambda/WLD -> dt; timer(n, N); n++) {
         updateH(WLD);
         mesR += fabs(poyntingZ(WLD, lengthY/2 + 250));
         mesT += fabs(poyntingZ(WLD, -lengthY/2 - 1000));
@@ -56,7 +57,7 @@ int main(int argc, char **argv) {
 
         if (N-n < WLD -> T) updatePhaser(WLD, P);
         if (!(n%( WLD->T ))) {
-            writeRow(WLD, "/RTA", WLD->dt*n/200, mesR, mesT, mesA, mesTotal);
+            writeRow(WLD, "/RTA", WLD->dt*n/nMultiplier, mesR, mesT, mesA, mesTotal);
             mesR = 0;
             mesT = 0;
             mesA = 0;
